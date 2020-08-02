39a68582abcd2bb7433531f5465cfa461cd8269f
Rebecca Taylor <bex@beccasaur.us>
2020-08-02 02:16:37 -0700

    mv spec.config.sh

--- examples/spec.config.sh
+++ examples/spec.config.sh
@@ -1,3 +1,12 @@
-spec.helperFilenames() {
-  echo $( ___spec___.helperFilenames ) fooHelper.sh
+spec.specFunctionPrefixes() {
+  echo test $(___spec___.specFunctionPrefixes)
+}
+
+spec.setupFunctionNames() {
+  echo config $(___spec___.setupFunctionNames)
+}
+
+spec.teardownFunctionNames() {
+  # support the built-in @teardown plus our own
+  echo cleanup $(___spec___.teardownFunctionNames)
 }
