32661f988c022e7fead2d9ce5ed835837cff97f4
Rebecca Taylor <bex@beccasaur.us>
2020-08-06 03:47:56 -0700

    🔬 v0.2.4

--- package.sh
+++ package.sh
@@ -2,6 +2,6 @@ name spec
 
 version "$( grep SPEC_VERSION= bin/spec | sed 's/SPEC_VERSION="\([0-9]\.[0-9]\.[0-9]\)"/\1/' )"
 
-description "Test specifications"
+description "🔬 Test specifications"
 
 exclude examples/ private/
